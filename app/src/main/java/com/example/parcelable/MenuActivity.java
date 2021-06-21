40f15aca56818380d7e8156ca3dff369eab3296a
Administrator <taegwan3857@gmail.com>
2021-06-21 14:30:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/parcelable/MenuActivity.java
@@ -0,0 +1,45 @@
+package com.example.parcelable;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Intent passedIntent = getIntent();
+        processIntent(passedIntent);
+
+    }
+
+    private void processIntent(Intent intent){
+        if (intent != null){
+            ArrayList<String> names = (ArrayList<String>) intent.getSerializableExtra("names");
+            if(names != null){
+                Toast.makeText(getApplicationContext(), "전달받은 이름 리스트 갯수: " + names.size(), Toast.LENGTH_LONG).show();
+            }
+            SimpleData data = (SimpleData) intent.getParcelableExtra("data");
+            if(data != null){
+                Toast.makeText(getApplicationContext(), "전달받은 Simple data: " + data.message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
